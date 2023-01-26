8d039af82ba970418e9e92205d31676caa714571
balintlehel <balint.lehel848@gmail.com>
2023-01-26 15:18:15 +0100

    docker setup and first lesson about classes

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,29 @@
 <?php
 
-phpinfo();
+require_once '../Transaction.php';
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+//Classes and Objects
+
+//Before method chaining
+//$transaction = new Transaction(100, 'asadasdasdas');
+//
+//$transaction->addTax(8);
+//$transaction->applyDiscount(10);
+//var_dump($transaction->getAmount());
+
+
+//After method chaining
+$amount = (new Transaction(100, 'Transaction 1'))
+    ->addTax(8)
+    ->applyDiscount(10)
+    ->getAmount();
+
+var_dump($amount);
+
+echo '<br>';
+
+$transaction = (new Transaction(100, 'Transaction 2'))
+    ->addTax(8)
+    ->applyDiscount(10);
+
+var_dump($transaction->getAmount());
